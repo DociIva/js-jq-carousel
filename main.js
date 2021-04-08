212931e239ffe51f24675104cadc036aa467c711
DociIva <doci.iva@hotmail.com>
2021-04-08 17:04:21 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+//console.log('Ciao');
+$(document).ready(function(){
+
+    // REFERENZE ()
+    var nextBtn = $('.next');
+    var prevBtn = $('.prev');
+
+    // navigare tra le foto con il mouse
+    // next
+    // (chi vogliamo), (l'evento) (f. vuota)
+    nextBtn.click(function(){
+       console.log('next');
+    })
+    // prev
+    prevBtn.click(function(){
+        console.log('prev');
+    })
+
+
+// Chiusara del $document
+})  
